d5f1314701ef5636ea9452c3d2572eeb6141b645
kazurayam <kazuaki.urayama@gmail.com>
2019-01-17 21:31:00 +0900

    update

--- Scripts/test/com.kazurayam.ksbackyard.junit/JUnitCustomKeywordsTestRuuner/Script1547725541242.groovy
+++ Scripts/test/com.kazurayam.ksbackyard.junit/JUnitCustomKeywordsTestRuuner/Script1547725541242.groovy
@@ -1,3 +1,6 @@
 import com.kazurayam.ksbackyard.junit.JUnitCustomKeywordsTest
+import com.kms.katalon.core.model.FailureHandling
 
-CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(JUnitCustomKeywordsTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(
+	JUnitCustomKeywordsTest.class,
+	FailureHandling.CONTINUE_ON_FAILURE)
