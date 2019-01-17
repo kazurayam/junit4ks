518ebe1db47269ebbb17b0cf37eccf3405211906
kazurayam <kazuaki.urayama@gmail.com>
2019-01-18 05:38:22 +0900

    may specify FailureHandling.OPTIONAL to make the log less verbose

--- Scripts/test/com.kazurayam.ksbackyard.junit/JUnitCustomKeywordsTestRuuner/Script1547725541242.groovy
+++ Scripts/test/com.kazurayam.ksbackyard.junit/JUnitCustomKeywordsTestRuuner/Script1547725541242.groovy
@@ -4,3 +4,11 @@ import com.kms.katalon.core.model.FailureHandling
 CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(
 	JUnitCustomKeywordsTest.class,
 	FailureHandling.CONTINUE_ON_FAILURE)
+
+	//FailureHandling.OPTIONAL)
+
+	/*
+	 * Here you may specify FailureHandling.OPTIONAL.
+	 * Then the log output will become less verbose. 
+	 * But the test case will PASS even if the JUnitCustomKeywordsTest failed internally.
+	 */
