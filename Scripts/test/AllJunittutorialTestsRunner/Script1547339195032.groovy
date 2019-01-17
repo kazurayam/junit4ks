b156ab1278308cc281fbe5fc0652f6d516df09ea
kazurayam <kazuaki.urayama@gmail.com>
2019-01-17 20:58:34 +0900

    indent lines of the innter stack trace

--- Scripts/test/AllJunittutorialTestsRunner/Script1547339195032.groovy
+++ Scripts/test/AllJunittutorialTestsRunner/Script1547339195032.groovy
@@ -1,6 +1,6 @@
 import junittutorial.CalculatorTest
 import junittutorial.GreeterTest
 
-CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(CalculatorTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywordsTest.runWithJUnitRunner'(CalculatorTest.class)
 
-CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(GreeterTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywordsTest.runWithJUnitRunner'(GreeterTest.class)
