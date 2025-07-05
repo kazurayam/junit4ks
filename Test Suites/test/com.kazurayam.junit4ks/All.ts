4823c700957f80b21ff5376722f9de994f4f8f27
kazurayam <kazuaki.urayama@gmail.com>
2025-07-05 18:38:20 +0900

    #10

---
+++ Test Suites/test/com.kazurayam.junit4ks/All.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c72eff69-5955-4498-9fb3-8b0d3d731601</testSuiteGuid>
+   <testCaseLink>
+      <guid>70e64e5d-8d0b-470e-8221-dc5fc2b3d143</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test/com.kazurayam.junit4ks/JUnitCustomKeywordsTestRuuner</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
--- Test Suites/test/junittutorial/All.ts
+++ Test Suites/test/junittutorial/All.ts
@@ -10,44 +10,30 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>dac816c8-2db1-4dc6-842e-b402cca0bc73</testSuiteGuid>
+   <testSuiteGuid>e0b50d27-078a-4808-9d16-ae40d02f42a8</testSuiteGuid>
    <testCaseLink>
-      <guid>6137d5f8-c1b7-4b1e-bba2-423e81bcde68</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/test/com.kazurayam.junit4ks/JUnitCustomKeywordsTestRuuner</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0529caf6-d317-4deb-9eae-45f524b29202</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/test/junittutorial/ASuiteRunner</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0736147d-16f1-4116-8bf8-83933c0c5874</guid>
+      <guid>848ceedd-c217-4a2c-b6b7-6d9e056f23ec</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/test/junittutorial/CalculatorTestRunner</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>6bb26afc-075b-40fc-a91c-2670d2a1f17f</guid>
+      <guid>4920e4ea-aeaf-475d-97bf-76a983da9af1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/test/junittutorial/CalculatorTestWithIgnoreRestRunner</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>98d13583-d2f1-4d9c-8b74-fdcc98994962</guid>
+      <guid>50c038dd-0ad1-4296-a748-ca4af2e25955</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/test/junittutorial/GreeterTestRunner</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d355e10d-e7b6-4f5b-8b0a-5ffef4a78aad</guid>
+      <guid>e78f4ded-b26a-4697-8e6e-e5d455e52914</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/test/junittutorial/StarImportProblemDemo</testCaseId>
