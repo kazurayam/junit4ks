61230191e4521471f658935504e2a9d2808c0884
kazurayam <kazuaki.urayama@gmail.com>
2025-07-13 22:50:58 +0900

    use Gradle plugin com.vanniktech.maven.publish 0.33.3

--- Include/scripts/groovy/junittutorial/CalculatorFailingTest.groovy
+++ Include/scripts/groovy/junittutorial/CalculatorFailingTest.groovy
@@ -4,16 +4,18 @@ import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertNotEquals
 import static org.junit.Assert.fail
 
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4.class)
 public class CalculatorFailingTest {
-	
+
 	/**
-	 * this test fails, no catching it
+	 * this test throws an Exception but not catching it
 	 */
+    @Ignore
 	@Test
 	void testFailNoCatch() {
 		int expected = 1
